cb6ea69265167d18f137f0a7c7cff33d5e7bdb1a
Krekno <yigiterenkarabas@gmail.com>
2026-04-21 23:46:14 +0300

    initial commit

---
+++ src/main/java/com/krekno/bookshopbackend/BookShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.krekno.bookshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krekno/bookshopbackend/BookShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krekno.bookshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

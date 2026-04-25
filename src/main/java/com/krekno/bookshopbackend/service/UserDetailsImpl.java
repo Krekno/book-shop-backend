51b7ee35b656d6522015d78e0a63751d99749f3e
Krekno <yigiterenkarabas@gmail.com>
2026-04-25 19:25:34 +0300

    add privilege list to UserDetailsImpl

--- src/main/java/com/krekno/bookshopbackend/service/UserDetailsImpl.java
+++ src/main/java/com/krekno/bookshopbackend/service/UserDetailsImpl.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.krekno.bookshopbackend.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.io.Serial;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -34,12 +35,10 @@ public class UserDetailsImpl implements UserDetails {
         this.authorities = authorities;
     }
 
-    // A static factory method to easily build this object from your database User entity
     public static UserDetailsImpl build(User user) {
-        // NOTE: If you add roles to your User entity later (e.g., ROLE_USER, ROLE_ADMIN),
-        // you would map those roles into GrantedAuthorities here.
-        // Since the previous User entity didn't include roles, we are passing an empty list.
-        Collection<? extends GrantedAuthority> authorities = Collections.emptyList();
+
+        List<GrantedAuthority> authorities =
+                List.of(new SimpleGrantedAuthority(user.getRole().name()));
 
         return new UserDetailsImpl(
                 user.getId(),
